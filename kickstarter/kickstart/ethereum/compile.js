c0ef9c31bda52f07c2a2bc7b97afc2f73889f164
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-07-05 23:56:35 +0530

    compile pase error

--- kickstarter/kickstart/ethereum/compile.js
+++ kickstarter/kickstart/ethereum/compile.js
@@ -32,7 +32,10 @@ const output = solc.compile(JSON.stringify(input)); // don't JSON.parse this
 //creating the build foldrer
 fs.ensureDirSync(buildPath); //if the folder does not exits this command creates it for us
 
-console.log(output);
+//console.log(output);
+const parsedOutput = JSON.parse(output);
+console.log(parsedOutput);
+
 //this loops over the contract and the outputs 2 files for each contract
 // for ( let contract in output) {  //a 'for' 'in' loop ittertes over the keys of an object
 //     fs.outputJsonSync(
@@ -46,7 +49,7 @@ console.log(output);
 //     fs.outputJsonSync(path.resolve(buildPath, `${contract}.json`), contracts[contract]);
 //   }
 // }
-for (let contractName in output.contracts['Campaign.sol']) {
+for (let contractName in parsedOutput34.contracts['Campaign.sol']) {
   fs.outputJsonSync(
     path.resolve(buildPath, `${contractName}.json`),
     output.contracts['Campaign.sol'][contractName]
