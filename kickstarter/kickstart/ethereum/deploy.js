a2c2f0a6d9a0deb157adc3244cbd2e8473739dc0
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-07-06 23:19:37 +0530

    deployment script added

---
+++ kickstarter/kickstart/ethereum/deploy.js
@@ -0,0 +1,38 @@
+// Load environment variables.
+require("dotenv").config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const {Web3}= require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
+const mnemonicPhrase = process.env.ACCOUNT_MNEMONIC;
+const network = process.env.SEPOLIA_ENDPOINT;
+
+const provider = new HDWalletProvider({
+  mnemonic: {
+    phrase: mnemonicPhrase
+  },
+  providerOrUrl: network
+});
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    console.log("Attempting to deploy from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(compiledFactory.abi)
+      .deploy({ data: "0x" + compiledFactory.evm.bytecode.object })
+      .send({ from: accounts[0] });
+
+    console.log("Contract deployed to", result.options.address);
+  } catch (err) {
+    console.error("Error during deployment:", err);
+  } finally {
+    provider.engine.stop(); // always stop the provider
+  }
+};
+
+deploy().catch((err) => {
+  console.error("Unhandled error in deploy():", err);
+});
