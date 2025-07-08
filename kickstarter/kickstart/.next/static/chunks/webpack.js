e7e48ee6132a210142c4aa8f7b89d3b7f7b88b5f
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-07-09 01:55:12 +0530

    added a card to the UI

--- kickstarter/kickstart/.next/static/chunks/webpack.js
+++ kickstarter/kickstart/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c64fde99f9242f0"; }
+/******/ 		__webpack_require__.h = function() { return "d880f5bc9d8d98c1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- kickstarter/kickstart/.next/static/webpack/webpack.5ebd12d7630d157d.hot-update.js
+++ kickstarter/kickstart/.next/static/webpack/webpack.5ebd12d7630d157d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7c64fde99f9242f0"; }
+/******/ 	__webpack_require__.h = function() { return "b4398bb33e0610be"; }
 /******/ }();
 /******/ 
 /******/ }
