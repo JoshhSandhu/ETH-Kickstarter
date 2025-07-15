7de0d19a1e16633aa04c36f40775d7ba3c090f17
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-07-15 10:27:31 +0530

    users can now create a request

--- kickstarter/kickstart/.next/static/webpack/webpack.7db68d6a76fc8439.hot-update.js
+++ kickstarter/kickstart/.next/static/webpack/webpack.7db68d6a76fc8439.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "e3daf737a2073cf1"; }
+/******/ 	__webpack_require__.h = function() { return "71474e0b419fc682"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
