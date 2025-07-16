9c494f77db4708e36ebbdb75c7a7d5af76ccc62a
Josh <50963558+JoshhSandhu@users.noreply.github.com>
2025-07-16 06:11:39 +0530

    users can now approve and finailze requests

--- kickstarter/kickstart/pages/campaigns/requests/index.js
+++ kickstarter/kickstart/pages/campaigns/requests/index.js
@@ -3,20 +3,16 @@ import Layout from "../../../components/layout";
 import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Campaign from "../../../ethereum/campaign";
-import requests from "../../../ethereum/requests";
 import RequestRow from "../../../components/RequestRow";
 
 
 class RequestIndex extends Component{
-    static async getInitialProps(props){
-        const { address } = props.query;
-        return { address };
-    }
 
     static async getInitialProps(props) {
         const { address } = props.query;
         const campaign = Campaign(address);
-        const requestCount = await campaign.methods.getRequestsCount().call();
+        const requestCount = await campaign.methods.getRequestsCounts().call();
+        const approversCount = await campaign.methods.approversCount().call();
 
         const requests = await Promise.all(
             Array(parseInt(requestCount)).fill().map((element, index) => {
@@ -35,7 +31,8 @@ class RequestIndex extends Component{
         return { 
         address, 
         requests: serializedRequests, 
-        requestCount: requestCount.toString()
+        requestCount: requestCount.toString(),
+        approversCount: approversCount.toString()
         };
     }
 
@@ -77,13 +74,15 @@ class RequestIndex extends Component{
                             <HeaderCell>Value</HeaderCell>
                             <HeaderCell>Recipient</HeaderCell>
                             <HeaderCell>Approval Count</HeaderCell>
-                            <HeaderCell>Complete</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
                         </Row>
                     </Header>
                     <Body>
                         {this.renderRow()}
                     </Body>
                 </Table>
+                <div>Found {requests.length} requests.</div>
             </Layout>
         );
     }
